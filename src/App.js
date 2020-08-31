9f127e00b91c78fadc9922fb5a96bb5717b1a716
VickeTrossing <vitro001@gmail.com>
2020-08-31 16:11:45 +0200

    Some smal CSS changes, I think...

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends React.Component{
       <div className="App">
         <header>
           <form id="to-do-form" onSubmit={this.addItem}>
-            <input type="text" placeholder="Enter task" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+            <input type="text" placeholder="Enter TODO" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
             <button type="submit">Add</button>
           </form>
           <p>{this.state.items.text}</p>
