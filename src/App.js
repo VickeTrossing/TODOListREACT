f4101fe76a394981cd8015415a0206bccdcdcfdc
VickeTrossing <vitro001@gmail.com>
2020-09-01 20:10:03 +0200

    Installed firebase, trying to config it

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import './App.css';
 import ListItems from './ListItems'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {faTrash} from '@fortawesome/free-solid-svg-icons' 
+import * as firebase from 'firebase';
+
+// var config = {
+//   apiKey: "AIzaSyDIW_W_0jdaffMD2yRdcljQo4WNkg3yZoU",
+//   authDomain: "todoreact-c486d.firebaseapp.com",
+//   databaseURL: "https://todoreact-c486d.firebaseio.com",
+//   projectId: "todoreact-c486d",
+//   storageBucket: "todoreact-c486d.appspot.com",
+//   messagingSenderId: "650739750861",
+//   appId: "1:650739750861:web:3767616263503f9f5a1684",
+//   measurementId: "G-0HRENKS3PY"
+// };
+
+
+// firebase.initializeApp(config);
 
 library.add(faTrash);
 
